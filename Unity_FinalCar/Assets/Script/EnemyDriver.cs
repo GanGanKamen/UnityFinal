12d70db76dd59d18b0fd51b22253f753526f19bb
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-07-24 17:27:20 +0900

    初期

---
+++ Unity_FinalCar/Assets/Script/EnemyDriver.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDriver : Driver
+{
+    const float ENGINE_TORQU = 0.7f;
+    const float STEERING = 0.5f;
+    const float DISTANCE_RANGE = 5.0f;
+
+    public GameMain gameMain;       //<- CheckPoint の配列を取得するため
+    public int indexCheckPoint;     //<- 配列から何番目のを取得するか？
+    public Vector3 AimCross;        // モニター 外積
+    public float AimDistance;       // モニター 距離
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        indexCheckPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // myCar が関連づいているか？
+        if (myCar == null ||
+        // ゲームが開始されたか？
+            GameMain.GamePhase != GameMain.eGamePhase.GAME){
+            if (myCar != null){
+                myCar.EngineTorqu = 0.0f;
+                myCar.BreakTorqu = 1.0f;
+            }
+            return;
+        }
+
+        // 向かうチェックポイントを取得する
+        CheckPoint aimPoint = 
+            gameMain.CheckPointList[indexCheckPoint];
+
+        // 目的に到達したか？を判断
+        AimDistance = 
+            Vector3.Distance(
+                myCar.transform.position, 
+                aimPoint.transform.position);
+                
+        if (AimDistance <= DISTANCE_RANGE){
+            // 次のチェックポイントへ切り替え
+            indexCheckPoint++;
+            if (indexCheckPoint >= gameMain.CheckPointList.Length){
+                // 最後までいったら最初から
+                indexCheckPoint = 0;
+            }
+        }
+
+        // アクセルを踏む
+        myCar.EngineTorqu = ENGINE_TORQU;
+        myCar.BreakTorqu = 0.0f;
+
+        // ステアリングを操作
+        Vector3 forward = myCar.transform.forward;
+        Vector3 direction = 
+            aimPoint.transform.position - myCar.transform.position;
+        AimCross = Vector3.Cross(forward, direction).normalized;
+        myCar.Steering = AimCross.y;
+//        myCar.Steering = (AimCross.y > 0) ? STEERING : -STEERING;
+
+    }
+}
---
+++ Unity_FinalCar/Assets/Script/PlayerDriver.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDriver : Driver {
+
+    public UnityEngine.UI.Slider sliderSteering;
+    public UnityEngine.UI.Slider sliderEngineTorqu;
+    public UnityEngine.UI.Button buttonSwitchView;
+    public UnityEngine.UI.Button buttonBreak;
+
+    public Camera MainCamera;
+    FollowCamera followCamera;
+
+    int currentView;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        if (MainCamera != null) {
+            followCamera = MainCamera.GetComponent<FollowCamera> ();
+        }
+
+        if (sliderSteering != null) {
+            sliderSteering.onValueChanged.AddListener (delegate (float newValue) {
+                // ステアリングのスライダーが更新されたとき
+                if (myCar != null) {
+                    myCar.Steering = newValue;
+                }
+            });
+        }
+
+        if (sliderEngineTorqu != null) {
+            sliderEngineTorqu.onValueChanged.AddListener (delegate (float newValue) {
+                // エンジントルクのスライダーが更新されたとき
+                if (myCar != null) {
+                    myCar.EngineTorqu = newValue;
+                }
+            });
+        }
+
+        if (buttonBreak != null) {
+            buttonBreak.onClick.AddListener (delegate {
+                // ブレーキを踏んだ
+                if (myCar != null) {
+                    myCar.BreakTorqu = 1.0f;
+                }
+            });
+        }
+
+        if (buttonSwitchView != null) {
+            buttonSwitchView.onClick.AddListener (delegate {
+                // 視点切り替え
+                currentView++;
+                if (currentView >= myCar.CameraPositions.Length) {
+                    currentView = 0;
+                }
+                SwitchView (currentView);
+            });
+        }
+
+        currentView = 1;
+        SwitchView (currentView);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (myCar != null && myCar.BreakTorqu > 0.0f) {
+            myCar.BreakTorqu -= Time.deltaTime;
+            if (myCar.BreakTorqu < 0.0f) {
+                myCar.BreakTorqu = 0.0f;
+            }
+        }
+    }
+
+    public void SwitchView (int viewNo)
+    {
+        if (viewNo < 0) {
+            viewNo = 0;
+        } else if (viewNo >= myCar.CameraPositions.Length) {
+            viewNo = myCar.CameraPositions.Length - 1;
+        }
+        if (followCamera != null && myCar.CameraPositions [viewNo] != null) {
+            followCamera.SetAim (myCar.CameraLookAt, myCar.CameraPositions [viewNo]);
+        }
+    }
+
+}
